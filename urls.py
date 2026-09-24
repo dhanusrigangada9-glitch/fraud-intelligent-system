d5e56759f736c06c28f9c8a887cbd8286582f8e2
dhanusrigangada9-glitch <dhanusrigangada9@gmail.com>
2026-09-24 18:13:28 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,17 +1,48 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 
 urlpatterns = [
 
     path(
-        "admin/",
-        admin.site.urls
+        "",
+        views.home,
+        name="home"
     ),
 
     path(
-        "",
-        include("detector.urls")
+        "predict/",
+        views.predict,
+        name="predict"
     ),
 
+    path(
+        "upload/",
+        views.upload_csv,
+        name="upload"
+    ),
+
+    path(
+        "search/",
+        views.search_transaction,
+        name="search_transaction"
+    ),
+
+    path(
+        "dashboard/",
+        views.dashboard,
+        name="dashboard"
+    ),
+
+    path(
+        "download-results/",
+        views.download_fraud_results,
+        name="download_fraud_results"
+    ),
+
+    path(
+        "download-dataset/",
+        views.download_dataset,
+        name="download_dataset"
+    ),
 ]
